4cf496cc603b24fe869c07adca4165be692f99cc
Yume <poppym0232@gmail.com>
2022-11-07 22:06:02 +0900

    仮のクライアント実装

--- AIDL_Client/app/src/main/java/com/example/aidl_client/MainActivity.kt
+++ AIDL_Client/app/src/main/java/com/example/aidl_client/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.example.aidl_client
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.Build
 import android.os.Bundle
+import android.os.IBinder
+import android.os.RemoteException
+import androidx.annotation.RequiresApi
+import com.example.aidl_client.databinding.ActivityMainBinding
+import com.example.aidl_service.IMyAidlInterface
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+    lateinit var binding: ActivityMainBinding
+    private var bond = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            if (bond) {
+                try {
+                    val text = iMyAidlInterface?.message
+                    binding.textView.text = text
+                } catch (e: RemoteException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val it = Intent("MyRemoteService")
+        it.setPackage("com.example.aidl_service")
+        bindService(it, connection, Context.BIND_AUTO_CREATE)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unbindService(connection)
+    }
+
+    private var iMyAidlInterface: IMyAidlInterface? = null
+
+    private val connection: ServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName, binder: IBinder) {
+            iMyAidlInterface = IMyAidlInterface.Stub.asInterface(binder)
+            bond = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            iMyAidlInterface = null
+            bond = false
+        }
     }
 }
